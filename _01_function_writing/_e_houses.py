945b7852be8b5393c350a353962259219b37de2c
Aarit Jayaswal <aaritrj@gmail.com>
2024-02-10 11:25:06 -0800

    ihg7   yfui

--- _01_function_writing/_e_houses.py
+++ _01_function_writing/_e_houses.py
@@ -3,9 +3,42 @@ Have the turtle draw a row of houses.
 """
 from tkinter import messagebox, simpledialog, Tk
 import turtle
+import random
+
+cheese = turtle.Turtle()
+cheese.penup()
 
 
 
+def draw_house(hight):
+    cheese.pendown()
+    cheese.left(90)
+    cheese.pencolor('black')
+    cheese.forward(hight)
+    cheese.right(90)
+    cheese.forward(100)
+    cheese.right(90)
+    cheese.forward(hight)
+    cheese.right(90)
+    cheese.forward(100)
+    cheese.right(90)
+    cheese.penup()
+    cheese.right(90)
+    cheese.forward(40)
+    cheese.left(90)
+    cheese.pendown()
+    cheese.forward(40)
+    cheese.right(90)
+    cheese.forward(20)
+    cheese.right(90)
+    cheese.forward(40)
+    cheese.penup()
+    cheese.right(90)
+    cheese.forward(90)
+    cheese.right(180)
+    cheese.pendown()
+    cheese.pencolor('green')
+    cheese.forward(160)
 
 if __name__ == '__main__':
     window = turtle.Screen()
@@ -14,25 +47,19 @@ if __name__ == '__main__':
     #   1) Move the turtle to the left side of the window near the bottom.
     #   2) Draw ONE flat-topped house with height=100 and green grass after it.
 
-    cheese = turtle.Turtle()
-    cheese.penup()
-    cheese.goto(-350,-350)
-    cheese.pendown()
-    cheese.left(90)
-    for i in range(4):
-        cheese.forward(100)
-        cheese.right(90)
-    cheese.penup()
-    cheese.goto(-310, -350)
-    cheese.pendown()
-    cheese.forward(40)
-    cheese.right(90)
-    cheese.forward(20)
-    cheese.right(90)
-    cheese.forward(40)
-    cheese.penup()
-    turtle.done()
 
+    cheese.goto(-350,-350)
+    for i in range(10):
+        g = random.randint(1, 3)
+        b = 1
+        if g == 1:
+            b = 60
+        elif g == 2:
+            b = 120
+        elif g == 3:
+            b = 250
+        draw_house(b)
+    turtle.done
     #   3) Put the code that drew the house into a function called draw_house
     #      HINT: Only the code that draws one house should go in this function.
     #   4) Using the function you just created, draw 10 houses.
