66b64fade591fb1bc9a442eea2d979d50bf57d57
Aarit Jayaswal <aaritrj@gmail.com>
2024-02-11 12:40:38 -0800

    thdfgftghrthft

--- _01_function_writing/_e_houses.py
+++ _01_function_writing/_e_houses.py
@@ -7,7 +7,42 @@ import random
 
 cheese = turtle.Turtle()
 cheese.penup()
-
+cheese.speed(0)
+def draw_pointy_house(hight):
+    cheese.pendown()
+    cheese.left(90)
+    cheese.pencolor('black')
+    cheese.forward(hight)
+    cheese.right(90)
+    cheese.forward(100)
+    cheese.left(135)
+    cheese.forward(71)
+    cheese.left(90)
+    cheese.forward(71)
+    cheese.left(135)
+    cheese.forward(100)
+    cheese.right(90)
+    cheese.forward(hight)
+    cheese.right(90)
+    cheese.forward(100)
+    cheese.right(90)
+    cheese.penup()
+    cheese.right(90)
+    cheese.forward(40)
+    cheese.left(90)
+    cheese.pendown()
+    cheese.forward(40)
+    cheese.right(90)
+    cheese.forward(20)
+    cheese.right(90)
+    cheese.forward(40)
+    cheese.penup()
+    cheese.right(90)
+    cheese.forward(90)
+    cheese.right(180)
+    cheese.pendown()
+    cheese.pencolor('green')
+    cheese.forward(160)
 
 
 def draw_house(hight):
@@ -39,7 +74,6 @@ def draw_house(hight):
     cheese.pendown()
     cheese.pencolor('green')
     cheese.forward(160)
-
 if __name__ == '__main__':
     window = turtle.Screen()
     window.bgcolor('white')
@@ -48,18 +82,21 @@ if __name__ == '__main__':
     #   2) Draw ONE flat-topped house with height=100 and green grass after it.
 
 
-    cheese.goto(-350,-350)
-    for i in range(10):
+    cheese.goto(-440,-390)
+    for i in range(7):
         g = random.randint(1, 3)
         b = 1
+        d = random.randint(1, 2)
         if g == 1:
             b = 60
         elif g == 2:
             b = 120
         elif g == 3:
             b = 250
-        draw_house(b)
-    turtle.done
+        if d == 1:
+            draw_pointy_house(b)
+        elif d == 2:
+            draw_house(b)
     #   3) Put the code that drew the house into a function called draw_house
     #      HINT: Only the code that draws one house should go in this function.
     #   4) Using the function you just created, draw 10 houses.
@@ -76,4 +113,4 @@ if __name__ == '__main__':
     #      (JUST the roof part): draw_pointy_roof, draw_flat_roof
     #   11) By calling the correct "roof" function, make large houses have
     #      flat roofs and all the others have pointy roofs.
-    pass
+    turtle.done()
